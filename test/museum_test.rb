c98e8606ff32c50cf49ec5f21be8436246befba6
Will Dunlap <dunlapww@gmail.com>
2020-09-10 09:48:00 -0600

    Add attribute test for patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -25,6 +25,7 @@ class MuseumTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
   end
 
   def test_it_can_add_exhibits
@@ -42,6 +43,7 @@ class MuseumTest < Minitest::Test
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
+
 
 
 
