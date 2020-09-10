99e1900ad652ec38651e749e64780878d39312fb
Will Dunlap <dunlapww@gmail.com>
2020-09-10 09:10:26 -0600

    Add exhibit attribute tests

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,8 +3,18 @@ require './lib/exhibit'
 
 class ExhibitTest < Minitest::Test
 
-  def test_it_exists
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    assert_instance_of Exhibit, exhibit
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
   end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_has_a_name_and_cost
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
+
 end
